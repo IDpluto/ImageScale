5ed1e51cf8ab57958b8f6304db1b314d52675ebc
idohyeon <d0013159@gmail.com>
2023-04-10 19:19:01 +0900

    UI & utils

---
+++ app/src/main/java/com/example/imagescale/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.imagescale;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_INFO = 1;
+    TextView tVResult, tVResultLabel;
+    String str;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnReq = (Button) findViewById(R.id.btnRequest);
+        Button btnEnd = (Button) findViewById(R.id.btnEnd);
+        EditText editTextId = findViewById(R.id.editTextID);
+        tVResultLabel = (TextView)findViewById(R.id.tVResultLabel);
+        tVResult = (TextView)findViewById(R.id.tVResult);
+        btnReq.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                str = editTextId.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), InformationActivity.class);
+                intent.putExtra("id", str);
+                startActivityForResult(intent, REQUEST_INFO);
+            }
+        });
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ( (requestCode == REQUEST_INFO) && (resultCode==RESULT_OK)) {
+            tVResultLabel.setText("전송\n정보\n출력");
+            str = "아이디: " + str;
+            str = str + "\n이름: " + data.getStringExtra("name");
+            str = str + "\n나이: " + data.getStringExtra("age");
+            str = str + "\n성별: " + data.getStringExtra("sex");
+            str = str + "\n자격증: " + data.getStringExtra("license");
+            tVResult.setText(str);
+        }
+    }
+}
